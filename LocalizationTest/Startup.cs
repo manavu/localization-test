6f76fd6b486c954157c695e794e52659a891ea64
Manabu Yoshida <yoshida_manabu@gyo-pro.co.jp>
2019-12-19 16:04:21 +0900

    .net core 2.2 で動作するように修正

--- LocalizationTest/Startup.cs
+++ LocalizationTest/Startup.cs
@@ -28,14 +28,50 @@ namespace LocalizationTest
             // add loacalization
             services.AddLocalization(options => options.ResourcesPath = "Resources");
 
+#if NETCOREAPP2_2
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix, options => options.ResourcesPath = "Resources");
+#else
             services.AddControllersWithViews()
                 .SetCompatibilityVersion(CompatibilityVersion.Latest)
                 .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix, options => options.ResourcesPath = "Resources");
+#endif
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+#if NETCOREAPP2_2
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            
+            var locOptions = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+            app.UseRequestLocalization(locOptions.Value);
+            
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+#else
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -45,10 +81,10 @@ namespace LocalizationTest
                 app.UseExceptionHandler("/Home/Error");
             }
 
+            app.UseStaticFiles();
+
             var locOptions = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
             app.UseRequestLocalization(locOptions.Value);
-
-            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -61,5 +97,6 @@ namespace LocalizationTest
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
+#endif
     }
 }
